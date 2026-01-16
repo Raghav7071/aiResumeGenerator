4a6609481260864e6b269791fc988b0aecfb5666
Raghav Yadav <raghavyadav4717@gmail.com>
2026-01-17 02:20:39 +0530

    Fix CTA button text and icon styling

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Banner from '../components/Home/Banner'
 import Hero from '../components/Home/Hero'
+import Feather from "../components/Home/Feature"
+import Testimonial from '../components/Home/Testimonial'
+import CallToAction from '../components/Home/CallToAction'
+
 
 const Home = () => {
   return (
     <div>
         <Banner />
-         <Hero />
+        <Hero />
+        <Feather />
+        <Testimonial />
+        <CallToAction />
+       
+         
     </div>
   )
 }
